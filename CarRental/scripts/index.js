654e6b3a43f2ad4f62761013ef02f2b8954a24b1
Erick Ortega <shut435@yahoo.com>
2024-05-02 14:19:43 -0400

    created function to determine car rental daily price

--- CarRental/scripts/index.js
+++ CarRental/scripts/index.js
@@ -1,8 +1,56 @@
 "use strict";
 
+const pickUpInput = Date(document.getElementById("pickUpInput"));
+const tollTagCheckBox = document.getElementById("tollTagCheckBox").checked;
+const gpsCheckbox = document.getElementById("gpsCheckBox").checked;
+const roadsideCheckBox = document.getElementById("roadsideCheckBox").checked;
+
+const under25 = document.getElementById("under25").checked;
+const over25 = document.getElementById("over25").checked;
+
 window.onload = function() {
-
-
+    
+    const estimateButton = document.getElementById("estimateButton");
+    
+    estimateButton.onclick = estimateTotal;
     
 }
 
+
+function estimateTotal(){
+    const numberOfDays = document.getElementById("numberOfDays").value;
+
+    let tollTagCheckBox = document.getElementById("tollTagCheckBox").checked;
+    let gpsCheckBox = document.getElementById("gpsCheckBox").checked;
+    let roadsideCheckBox  = document.getElementById("roadsideCheckBox").checked;
+
+
+    // calculate Car rental Alone
+    // basic rental is $29.99/day
+    let carRental = (numberOfDays * 29.99);
+    let dailyTollTagCharge, dailyGpsCharge, dailyRoadsideCharge, totalOptions;
+
+    if (tollTagCheckBox){
+        dailyTollTagCharge = (numberOfDays * 3.95);
+        carRental += dailyTollTagCharge;
+        totalOptions += dailyTollTagCharge;
+    }
+
+    if (gpsCheckBox){
+        dailyGpsCharge = (numberOfDays * 2.95);
+        carRental += dailyGpsCharge;
+        totalOptions += dailyGpsCharge
+    }
+
+    if (roadsideCheckBox){
+        dailyRoadsideCharge = (numberOfDays * 2.95);
+        carRental += dailyRoadsideCharge;
+        totalOptions += dailyRoadsideCharge;
+    }
+
+
+    // if the user is UNDER 25, charge 30% surcharge
+
+    console.log("\nCar Rental: " + carRental + "\nOptions: " + typeof totalOptions);
+
+}
